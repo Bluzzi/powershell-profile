d7a4695ee632c38fae9f5c6a2820e4634c81195b
Bluzzi <camilledugas.pro@gmail.com>
2025-11-17 02:06:51 +0100

    update Zoxide extension config

--- Core/Extensions/Zoxide.ps1
+++ Core/Extensions/Zoxide.ps1
@@ -1,5 +1,7 @@
 Invoke-Expression (& {
-  $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+  # $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
   
-  (zoxide init --hook $hook powershell | Out-String)
+  # (zoxide init --hook $hook powershell | Out-String)
+
+  Invoke-Expression (& { (zoxide init powershell | Out-String) })
 })
